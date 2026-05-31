c9fa2ff80428ea961ac822d155477350815b54b9
srilatha2012 <srilathp2012@gmail.com>
2026-05-31 12:47:48 -0400

    Added User and Family models

---
+++ backend/models/Family.js
@@ -0,0 +1,33 @@
+//Import mongoose package
+const mongoose = require("mongoose");
+
+const familySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    members: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        role: {
+            type: String,
+            enum: ["parent", "spouse", "teen", "child"],
+            required: true,
+        }
+    }],
+
+},
+    { timestamps: true }
+);
+
+const Family = mongoose.model("Family", familySchema);
+//Create User module with userSchema
+
+module.exports = Family;
---
+++ backend/models/User.js
@@ -0,0 +1,60 @@
+//Import mongoose package
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const saltRounds = process.env.SALT_ROUNDS;
+
+//Create user schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [
+            /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            "Please enter a valid email address",
+        ],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+
+    role: {
+        type: String,
+        enum: ["parent", "spouse", "teen", "child"],
+        default: "parent"
+    },
+    family: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Family"
+    },
+},
+    { timestamps: true }
+
+);
+//Create User module with userSchema
+const User = mongoose.model("User", userSchema);
+
+//Hash password before saving a new user or when password is changed
+userSchema.pre("save", async function() {
+    if(this.isNew || this.isModified("password")) {
+        this.password= await bcrypt.hash(this.password, saltRounds);
+    }
+});
+
+//Compare Login password with the hashed password stored in MongoDB
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password); //Here don't need await, because it already returns a Promise
+};
+
+
+module.exports = User;

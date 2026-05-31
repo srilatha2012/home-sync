8be59e229a952de0a213dd9bc6d4ae7b6703a6f9
srilatha2012 <srilathp2012@gmail.com>
2026-05-31 19:50:20 -0400

    Implemented user Login process

--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -1,31 +1,64 @@
 //Import dependencies
 const userRoutes = require("express").Router();
 
-const { User} = require("../../models");
-//POST request
-userRoutes.post("/register", async (req,res) =>{
-try{
-    const {username, email, password, role} = req.body;
-    const user = await User.create({
-        username,
-        email,
-        password,
-        role
-    });
-    const responseData = {
-        username: user.username,
-        email: user.email,
-        id: user._id,
+const { User } = require("../../models");
+const { signToken } = require("../../utils/auth");
 
-    }
-    res.status(201).json({
-        message: "User added successfully",
-        user: responseData
+//POST request  - User Registration
+userRoutes.post("/register", async (req, res) => {
+    try {
+        const { username, email, password, role } = req.body;
+        const user = await User.create({
+            username,
+            email,
+            password,
+            role
+        });
+        const responseData = {
+            username: user.username,
+            email: user.email,
+            id: user._id,
 
-    });
-    } catch(error){
+        }
+        res.status(201).json({
+            message: "User added successfully",
+            user: responseData
+
+        });
+    } catch (error) {
         res.status(400).json(error);
     }
 });
+
+//POST request -User Login
+userRoutes.post("/login", async (req, res) => {
+    try {
+
+        const { username, email, password } = req.body;
+        const user = await User.findOne({ username: username, email: email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        const isCorrectPassword = await user.isCorrectPassword(password);
+        if (!isCorrectPassword) {
+            return res.status(401).json({ message: "Invalid email or Password" });
+        }
+        const token = signToken(user);
+        res.status(200).json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Login Failed", error: error.message });
+    }
+});
+
 
 module.exports = userRoutes;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,6 +1,10 @@
-const jsonwebtoken = require("jsonwebtoken");
-const JSON_SECRET = process.env.JSON_SECRET;
-
-async function token(payload) {
-    jsonwebtoken.sign()
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+const expiration = "2h";
+module.exports = {
+    signToken : function ({username, email, _id}) {
+        const payload = {username, email, _id};
+        console.log("json111 secret", JWT_SECRET);
+         return jwt.sign(payload, JWT_SECRET, {expiresIn: expiration});
+    }
 }

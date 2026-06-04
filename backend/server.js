9e44bfa7b0e973f060f0aceec49161c752fe7b9b
srilatha2012 <srilathp2012@gmail.com>
2026-06-04 18:04:28 -0400

    Update CORS for Render Deployment

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Middleware
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
-}))
+}));
 
 app.use(express.json());
 

d41d710229a79800a619fd610a54e976d2b73054
srilatha2012 <srilathp2012@gmail.com>
2026-06-05 12:34:16 -0400

    added comment to config.ts file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,5 @@
+/*
+This configuration file allows the frontend to communicate with either the local backend or the deployed backend 
+*/
 export const API_URL =
   import.meta.env.VITE_API_URL || "http://localhost:3000";
